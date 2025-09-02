d2e5cf672337cfb771877b0ef2ab2497b3afb18a
tal96 <tal96@cornell.edu>
2025-09-02 14:13:40 -0400

    Update RegIncr

--- sim/tut3_verilog/regincr/regincr-adhoc-test.py
+++ sim/tut3_verilog/regincr/regincr-adhoc-test.py
@@ -30,11 +30,14 @@ input_values.extend( [0]*3 )
 # insert code here for constructing and elaborating a RegIncr model.
 # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
+model = RegIncr()
+model.elaborate()
+
 # Apply the Verilog import passes and the default pass group
 
 model.apply( VerilogPlaceholderPass() )
 model = VerilogTranslationImportPass()( model )
-model.apply( DefaultPassGroup() )
+model.apply( DefaultPassGroup(linetrace=True,textwave=True,vcdwave="regincr-adhoc-test") )
 
 # Reset simulator
 
@@ -57,3 +60,4 @@ for input_value in input_values:
 
   model.sim_tick()
 
+#model.print_textwave()
